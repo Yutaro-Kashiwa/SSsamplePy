cbfe25d225a9f3395e20179710f213557520b005
Yutaro Kashiwa <kashiwa.yutaro@g.wakayama-u.jp>
2022-08-23 13:24:46 +0900

    recheck

--- test/test_CSVPrinter.py
+++ test/test_CSVPrinter.py
@@ -4,9 +4,18 @@ from ss2022.CSVPrinter import CSVPrinter
 
 
 class TestCSVPrinter(unittest.TestCase):
-    def setUp(self):
-        self.printer = CSVPrinter("sample.csv")
+    @classmethod
+    def setUpClass(cls):
+        print("setUpClass")
 
     def test_read(self):
         l = self.printer.read()
         self.assertEqual(2, len(l))
+        print(1)
+
+
+    def test_read(self):
+        print(2)
+
+    def tearDown(self) :
+        print("tearDown")
